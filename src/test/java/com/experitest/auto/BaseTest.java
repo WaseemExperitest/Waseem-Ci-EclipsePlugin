d5f10c4dcb31c8b29af6d691fa445ab274035569
Wasewm Suleiman (Experitest) <waseem.suleiman_p@experitest.com>
2017-12-26 19:56:09 +0200

    cloudProperties

--- src/test/java/com/experitest/auto/BaseTest.java
+++ src/test/java/com/experitest/auto/BaseTest.java
@@ -19,12 +19,13 @@ public class BaseTest {
 		dc.setCapability("reportDirectory", "reports");
 		dc.setCapability("reportFormat", "xml");
 		String accessKey = getProperty("accessKey", cloudProperties);
-		if(accessKey != null && !accessKey.isEmpty()){
+		if (accessKey != null && !accessKey.isEmpty()) {
 			dc.setCapability("accessKey", getProperty("accessKey", cloudProperties));
 		} else {
 			dc.setCapability("user", getProperty("username", cloudProperties));
-			dc.setCapability("password", getProperty("password", cloudProperties));		}
-		
+			dc.setCapability("password", getProperty("password", cloudProperties));
+		}
+
 		// In case your user is assign to a single project leave empty,
 		// otherwise please specify the project name
 		dc.setCapability("project", getProperty("project", cloudProperties));
@@ -43,9 +44,15 @@ public class BaseTest {
 	}
 
 	private void initCloudProperties() throws FileNotFoundException, IOException {
-		FileReader fr = new FileReader("cloud.properties");
-		cloudProperties.load(fr);
-		fr.close();
+		File cloudPropertiesFile = new File("cloud.properties");
+		if (cloudPropertiesFile.exists()) {
+			FileReader fr = new FileReader("cloud.properties");
+			cloudProperties.load(fr);
+			fr.close();
+		}else {
+			cloudProperties.setProperty("accessKey", System.getenv("accessKey"));
+			cloudProperties.setProperty("url", System.getenv("url"));
+		}
 	}
 
 	private static synchronized String adhocDevice(String deviceQuery) {
